5d40c3e7875c27f631063080bffc202bee51143a
a13xand9r <a13x.student@yandex.ru>
2021-09-24 11:59:20 +0300

    deploy aki client check

--- src/App.tsx
+++ src/App.tsx
@@ -50,25 +50,25 @@ function App() {
   }, [])
 
   const onStartGame = async () => {
-    dispatch(actions.setFetching(true))
-    // setAki(await runAkinator())
-    // if (aki) {
-    //   dispatch(actions.setNewQuestion(
-    //     aki.question as string,
-    //     aki.answers as string[],
-    //     aki.currentStep,
-    //     aki.progress
-    //   ))
-    //   dispatch(actions.startGame())
-    // }
-    assistantRef.current?.sendAction({ type: 'START_GAME', payload: {} })
+    // dispatch(actions.setFetching(true))
+    setAki(await runAkinator())
+    if (aki) {
+      dispatch(actions.setNewQuestion(
+        aki.question as string,
+        aki.answers as string[],
+        aki.currentStep,
+        aki.progress
+      ))
+      dispatch(actions.startGame())
+    }
+    // assistantRef.current?.sendAction({ type: 'START_GAME', payload: {} })
   }
   const onAnswerClick = async (index: number) => {
-    dispatch(actions.setFetching(true))
-    // if (aki){
-    //   nextStep(dispatch, aki, index as AnswerType, wrongPersonId)
-    // }
-    assistantRef.current?.sendAction({ type: 'USER_ANSWER', payload: { answer: index } })
+    // dispatch(actions.setFetching(true))
+    if (aki){
+      nextStep(dispatch, aki, index as AnswerType, wrongPersonId)
+    }
+    // assistantRef.current?.sendAction({ type: 'USER_ANSWER', payload: { answer: index } })
   }
   const onBackClick = () => {
     dispatch(actions.setFetching(true))
